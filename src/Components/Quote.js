def18ff2eb9ab5fbb921e09767db2c41b06ac87d
Manoj Kumar Gangula <mkgangula1@gmail.com>
2024-12-08 03:38:20 +0530

    Screenshot feature completed

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -5,7 +5,6 @@ const Quote = forwardRef((props, ref) => {
   const [quote, setQuote] = useState("Never Quit!");
   const [author, setAuthor] = useState("Unknown");
   const quoteApiUrl = "/api/random";
-  const screenShotRef = useRef();
 
   async function getQuoteDetails() {
     try {
@@ -13,6 +12,7 @@ const Quote = forwardRef((props, ref) => {
       let data = await quoteResponse.json();
       setQuote(data[0].q);
       setAuthor(data[0].a);
+      props.updateQuoteInParent(data[0].q + " -" + data[0].a);
     } catch (error) {
       console.log(error);
     }
@@ -21,28 +21,11 @@ const Quote = forwardRef((props, ref) => {
   useImperativeHandle(ref, () => {
     return {
       getQuoteDetails,
-      captureScreenshot,
     };
   });
 
-  const captureScreenshot = () => {
-    let canvasPromise = html2canvas(screenShotRef.current);
-    canvasPromise.then((canvas) => {
-      let dataURL = canvas.toDataURL("image/png");
-
-      // Create a link element
-      let a = document.createElement("a");
-      // Set the href of the link to the data URL of the image
-      a.href = dataURL;
-      // Set the download attribute of the link
-      a.download = "screenshot.png";
-      a.click();
-    });
-  };
-
   return (
-    <div className="quote" ref={screenShotRef}>
-      <p>Hello</p>
+    <div className="quote" id="quote">
       <q>{quote}</q>
       <p>- {author}</p>
     </div>
--- src/Components/QuoteContainer.js
+++ src/Components/QuoteContainer.js
@@ -2,27 +2,44 @@ import { useEffect, useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./QuoteContainer.css";
 import Quote from "./Quote";
+import html2canvas from "html2canvas";
 
 function QuoteContainer() {
-  const quote = "Never Quit!";
-  const author = "Unknown";
-  const tweetContent = quote + " - " + author;
+  const [tweetContent, setTweetContent] = useState("Never Quit! - Unknown");
   const childRef = useRef();
   useEffect(() => {
     handleNewQuoteClick();
   }, []);
 
+  function updateQuoteInParent(updatedContent) {
+    setTweetContent(updatedContent);
+  }
   function handleNewQuoteClick() {
     if (childRef.current) childRef.current.getQuoteDetails();
   }
 
   function handleScreenshotButton() {
-    if (childRef.current) childRef.current.captureScreenshot();
+    let canvasPromise = html2canvas(document.getElementById("quote"), {
+      scale: 2,
+      backgroundColor: null,
+    });
+    canvasPromise
+      .then((canvas) => {
+        let dataURL = canvas.toDataURL("image/png");
+        let a = document.createElement("a");
+        a.href = dataURL;
+        a.download = "screenshot.png";
+        a.click();
+      })
+      .catch((err) => {
+        console.log("Not able to capture!");
+        console.log(err);
+      });
   }
 
   return (
     <div className="quoteContainer">
-      <Quote ref={childRef} />
+      <Quote ref={childRef} updateQuoteInParent={updateQuoteInParent} />
       <div className="options">
         <div>
           <a
